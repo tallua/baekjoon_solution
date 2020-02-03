c99b5747036c8046025d4f5110ece38dc76fc3ca
tallua <talluay@gmail.com>
2020-02-03 21:41:23 +0900

    .

--- 9426/main.cpp
+++ 9426/main.cpp
@@ -1,70 +1,227 @@
 #include <iostream>
-#include <set>
+#include <vector>
 #include <deque>
 
 using namespace std;
 
-struct my_set
+template<typename _Type, class _ReduceFn>
+class segment_tree
 {
-    multiset<uint16_t> left;
-    multiset<uint16_t> right;
+public:
+    using value_type = _Type;
+    using size_type = std::size_t;
 
-    void insert(uint16_t val)
+private:
+    using depth_type = std::size_t;
+    using reduce_func_type = _ReduceFn;
+
+private:
+    const value_type _default_value;
+    const reduce_func_type _reduce_fn;
+    std::vector<std::vector<value_type>> _data;
+
+public:
+    constexpr segment_tree(size_type N,
+                 const value_type& default_value = _Type(),
+                 const reduce_func_type& reduce_fn = reduce_func_type())
+        : _default_value(default_value), _reduce_fn(reduce_fn)
     {
-        if (left.empty())
-            left.insert(val);
-        else
+        do
         {
-            if (val <= *left.rbegin())
-                left.insert(val);
+            _data.push_back({});
+            _data.back().resize(N, _default_value);
+
+            N = N == 1 ? 0 : (N + 1) >> 1;
+        } while (N > 0);
+    }
+
+    template <class _ForwardIt>
+    segment_tree(_ForwardIt begin, _ForwardIt end,
+                 const value_type& default_value = _Type(),
+                 const reduce_func_type& reduce_fn = reduce_func_type())
+        : _default_value(default_value), _reduce_fn(reduce_fn)
+    {
+        _data.push_back({});
+        _data[0].insert(_data[0].begin(), begin, end);
+
+        size_type N = _data.back().size();
+        N = N == 1 ? 0 : (N + 1) >> 1;
+
+        while (N > 0)
+        {
+            _data.push_back({});
+            _data.back().resize(N, _default_value);
+            vector<value_type>& prev = _data[_data.size() - 2];
+
+            size_type n = 0;
+            while (n < N - 1)
+            {
+                _data.back()[n] = _reduce_fn(prev[n << 1], prev[(n << 1) + 1]);
+                ++n;
+            }
+
+            if ((n << 1) + 1 < prev.size())
+                _data.back()[n] = _reduce_fn(prev[n << 1], prev[(n << 1) + 1]);
             else
-                right.insert(val);
+                _data.back()[n] = prev[n << 1];
 
-            _balance_one();
+            N = N == 1 ? 0 : (N + 1) >> 1;
         }
     }
 
-    void erase(uint16_t val)
+public:
+    constexpr size_type size() const
     {
-
-        if (val <= *left.rbegin())
-        {
-            auto it = left.find(val);
-            left.erase(it);
-        }
-        else
-        {
-            auto it = right.find(val);
-            right.erase(it);
-        }
-
-        _balance_one();
+        return _data[0].size();
     }
 
-    void _balance_one()
+    value_type& get(size_type index)
     {
-        if (left.size() < right.size())
+        return _data[0].at(index);
+    }
+
+    const value_type& get(size_type index) const
+    {
+        return _data[0].at(index);
+    }
+
+    value_type& back()
+    {
+        return _data[0].back();
+    }
+
+    const value_type& back() const
+    {
+        return _data[0].back();
+    }
+
+public:
+    void update(size_type index, value_type value)
+    {
+        if (_data[0].size() <= index)
+            return;
+
+        _data[0][index] = value;
+
+        index = index >> 1;
+        depth_type depth = 1;
+        while (depth < _data.size())
         {
-            auto rotate = right.begin();
-            left.insert(*rotate);
-            right.erase(rotate);
-        }
-        else if (left.size() > right.size() + 1)
-        {
-            auto rotate = --left.end();
-            right.insert(*rotate);
-            left.erase(rotate);
+            const auto _child_left = index << 1;
+            const auto _child_right = _child_left + 1;
+
+            _data[depth][index] = _data[depth - 1][_child_left];
+            if (_child_right < _data[depth - 1].size())
+                _data[depth][index] = _reduce_fn(_data[depth][index], _data[depth - 1][_child_right]);
+
+            index = index >> 1;
+            depth += 1;
         }
     }
 
-    uint16_t mid() const
+    value_type query_range(size_type begin, size_type end) const
     {
-        return *left.crbegin();
+        if (end <= begin)
+            return _default_value;
+        if (_data[0].size() < end)
+            return _default_value;
+
+        return _query_range(begin, end - 1, 0);
     }
 
-    size_t size() const
+    template <class _Query>
+    size_type index_query(_Query query) const
     {
-        return left.size() + right.size();
+        size_type index = 0;
+        depth_type depth = _data.size() - 1;
+        while (depth != 0)
+        {
+            const size_type _child_left = index << 1;
+            const size_type _child_right = _child_left + 1;
+
+            if (_data[depth - 1].size() <= _child_right)
+            {
+                if (query(_data[depth - 1][_child_left], _default_value))
+                {
+                    return size();
+                }
+                else
+                {
+                    index = _child_left;
+                }
+            }
+            else
+            {
+                if (query(_data[depth - 1][_child_left], _data[depth - 1][_child_right]))
+                {
+                    index = _child_right;
+                }
+                else
+                {
+                    index = _child_left;
+                }
+            }
+
+            depth--;
+        }
+
+        return index;
+    }
+private:
+    /// note : front and back are inclusive range
+    value_type _query_range(size_type front, size_type back, depth_type depth) const
+    {
+        if (front == back)
+            return _data[depth][front];
+
+        value_type result = _default_value;
+
+        if (front % 2 != 0)
+        {
+            result = _reduce_fn(result, _data[depth][front]);
+            front += 1;
+        }
+
+        if (back % 2 == 0)
+        {
+            result = _reduce_fn(result, _data[depth][back]);
+            back -= 1;
+        }
+
+        const size_type half_front = front >> 1;
+        const size_type half_back = back >> 1;
+
+        if (half_front <= half_back)
+            result = _reduce_fn(result, _query_range(half_front, half_back, depth + 1));
+
+        return result;
+    }
+};
+
+
+template <typename _Type>
+struct sum
+{
+    const _Type operator() (const _Type& lhs, const _Type& rhs) const
+    {
+        return lhs + rhs;
+    }
+};
+
+template <typename _Type>
+struct nth_index
+{
+    _Type nth;
+
+    bool operator() (_Type lhs, _Type rhs)
+    {
+        if (nth <= lhs)
+        {
+            return false;
+        }
+
+        nth -= lhs;
+        return true;
     }
 };
 
@@ -77,26 +234,30 @@ int main(int argc, char** argv)
     int N, K;
     cin >> N >> K;
 
-    my_set temperatures;
-    deque<uint16_t> input_q;
+    segment_tree<int, sum<int>> temperatures(65536, 0);
+    deque<int> input_q;
+    int half_k = (K + 1) / 2;
     long long sum = 0;
     for (int n = 0; n < N; ++n)
     {
-        uint16_t current;
+        int current;
         cin >> current;
 
         input_q.push_back(current);
-        temperatures.insert(current);
 
-        if (temperatures.size() > K)
+        temperatures.update(current, temperatures.get(current) + 1);
+
+        if (input_q.size() > K)
         {
-            temperatures.erase(input_q.front());
+            int front = input_q.front();
             input_q.pop_front();
+
+            temperatures.update(front, temperatures.get(front) - 1);
         }
 
-        if (temperatures.size() == K)
+        if (input_q.size() == K)
         {
-            sum += temperatures.mid();
+            sum = sum + (long long)(temperatures.index_query(nth_index<int>{ half_k }));
         }
     }
 
