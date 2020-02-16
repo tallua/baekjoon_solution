2eed1d2d37f39d770fdb3d3f8f1dbc9c9f7ead5f
tallua <talluay@gmail.com>
2020-02-16 17:11:40 +0900

    .

--- 1220/main.cpp
+++ 1220/main.cpp
@@ -19,7 +19,7 @@ struct rect_t
 
 struct astar_task
 {
-    coord_t x, y;
+    size_t x, y;
     cost_t actual;
     cost_t estimate;
 
@@ -36,41 +36,55 @@ struct task_sort
 void parse_inputs(vector<rect_t>& departs, vector<coord_t>& indx_to_x, vector<coord_t>& indx_to_y,
                   coord_t a, coord_t b, coord_t c, coord_t d)
 {
-    set<coord_t> x_coord_set;
-    set<coord_t> y_coord_set;
     for (size_t n = 0; n < departs.size(); ++n)
     {
         cin >> departs[n].lx >> departs[n].ly >> departs[n].hx >> departs[n].hy >> departs[n].cost;
     }
 
+    vector<coord_t> x_coord_set;
+    x_coord_set.reserve(departs.size() + 2);
+    vector<coord_t> y_coord_set;
+    y_coord_set.reserve(departs.size() + 2);
     for (const auto& depart : departs)
     {
-        x_coord_set.insert(depart.lx);
-        x_coord_set.insert(depart.hx);
-        y_coord_set.insert(depart.ly);
-        y_coord_set.insert(depart.hy);
+        x_coord_set.push_back(depart.lx);
+        x_coord_set.push_back(depart.hx);
+        y_coord_set.push_back(depart.ly);
+        y_coord_set.push_back(depart.hy);
     }
 
-    x_coord_set.insert(a);
-    y_coord_set.insert(b);
-    x_coord_set.insert(c);
-    y_coord_set.insert(d);
-    x_coord_set.insert(0);
-    y_coord_set.insert(0);
-    x_coord_set.insert(100000000);
-    y_coord_set.insert(100000000);
+    x_coord_set.push_back(a);
+    y_coord_set.push_back(b);
+    x_coord_set.push_back(c);
+    y_coord_set.push_back(d);
+    x_coord_set.push_back(100000000);
+    y_coord_set.push_back(100000000);
+
+    sort(x_coord_set.begin(), x_coord_set.end());
+    sort(y_coord_set.begin(), y_coord_set.end());
 
     indx_to_x.reserve(x_coord_set.size());
     indx_to_y.reserve(y_coord_set.size());
+    indx_to_x.push_back(0);
+    indx_to_y.push_back(0);
 
     for (const auto& x_val : x_coord_set)
-        indx_to_x.push_back(x_val);
+    {
+        if (indx_to_x.back() != x_val)
+            indx_to_x.push_back(x_val);
+    }
     for (const auto& y_val : y_coord_set)
-        indx_to_y.push_back(y_val);
+    {
+        if (indx_to_y.back() != y_val)
+            indx_to_y.push_back(y_val);
+    }
 }
 
 int main(int argc, char** argv)
 {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
     coord_t start_x, start_y, end_x, end_y;
     cin >> start_x >> start_y >> end_x >> end_y;
 
@@ -103,27 +117,23 @@ int main(int argc, char** argv)
         return y * grid_x_size + x;
     };
 
-    vector<vector<cost_t>> horz_costs;
-    horz_costs.resize(grid_y_size);
-    for (auto& r : horz_costs)
-        r.resize(grid_x_size - 1, 10);
-    vector<vector<cost_t>> vert_costs;
-    vert_costs.resize(grid_y_size - 1);
-    for (auto& r : vert_costs)
-        r.resize(grid_x_size, 10);
+    vector<cost_t> horz_costs;
+    horz_costs.resize(grid_y_size * grid_x_size, 10);
+    vector<cost_t> vert_costs;
+    vert_costs.resize(grid_y_size * grid_x_size, 10);
 
     for (const auto& rect : departs)
     {
-        const size_t lx = x_to_indx[rect.lx];
-        const size_t ly = y_to_indx[rect.ly];
-        const size_t hx = x_to_indx[rect.hx];
-        const size_t hy = y_to_indx[rect.hy];
+        const size_t& lx = x_to_indx[rect.lx];
+        const size_t& ly = y_to_indx[rect.ly];
+        const size_t& hx = x_to_indx[rect.hx];
+        const size_t& hy = y_to_indx[rect.hy];
 
         for (size_t y = ly + 1; y < hy; ++y)
         {
             for (size_t x = lx; x < hx; ++x)
             {
-                horz_costs[y][x] = rect.cost;
+                horz_costs[calc_indx(y, x)] = rect.cost;
             }
         }
 
@@ -131,7 +141,7 @@ int main(int argc, char** argv)
         {
             for (size_t y = ly; y < hy; ++y)
             {
-                vert_costs[y][x] = rect.cost;
+                vert_costs[calc_indx(y, x)] = rect.cost;
             }
         }
     }
@@ -140,13 +150,17 @@ int main(int argc, char** argv)
     vector<bool> visited;
     visited.resize(grid_y_size * grid_x_size, false);
 
-    auto dist_to_end = [&](coord_t x, coord_t y) -> cost_t {
-        return (abs((cost_t)end_x - x) + abs((cost_t)end_y - y)) * 10;
+    auto cost_to_end = [&](const size_t& x, const size_t& y) -> cost_t {
+        return (abs(end_x - indx_to_x[x]) + abs(end_y - indx_to_y[y])) * 10;
     };
 
     // a* with priority queue
+    const size_t start_indx_x = x_to_indx[start_x];
+    const size_t start_indx_y = y_to_indx[start_y];
+    const size_t end_indx_x = x_to_indx[end_x];
+    const size_t end_indx_y = y_to_indx[end_y];
     priority_queue<astar_task, vector<astar_task>, task_sort> pq;
-    pq.push({ start_x, start_y, 0LL, dist_to_end(start_x, start_y) });
+    pq.push({ start_indx_x, start_indx_y, 0LL, cost_to_end(start_indx_x, start_indx_y) });
 
     cost_t result = 0;
     while (true)
@@ -155,50 +169,55 @@ int main(int argc, char** argv)
         pq.pop();
         //cout << "mark : " << task.x << " " << task.y << 
         //    " : " << task.actual << " | " << task.estimate << endl;
-        if (task.x == end_x && task.y == end_y)
+        if (task.x == end_indx_x && task.y == end_indx_y)
         {
             result = task.actual;
             break;
         }
 
-        const size_t cur_x = x_to_indx[task.x];
-        const size_t cur_y = y_to_indx[task.y];
-
-        if (visited[calc_indx(cur_y, cur_x)])
+        if (visited[calc_indx(task.y, task.x)])
             continue;
-        visited[calc_indx(cur_y, cur_x)] = true;
+        visited[calc_indx(task.y, task.x)] = true;
 
         // up
-        if (cur_y != grid_y_size - 1 && !visited[calc_indx(cur_y + 1, cur_x)])
+        if (task.y != grid_y_size - 1 && !visited[calc_indx(task.y + 1, task.x)])
         {
-            coord_t next_x = task.x;
-            coord_t next_y = indx_to_y[cur_y + 1];
-            cost_t actual = task.actual + vert_costs[cur_y][cur_x] * abs(next_y - task.y);
-            pq.push({ next_x, next_y, actual, actual + dist_to_end(next_x, next_y) });
+            const size_t next_x = task.x;
+            const size_t next_y = task.y + 1;
+            const cost_t cost = vert_costs[calc_indx(task.y, task.x)];
+            const coord_t dist = abs(indx_to_y[next_y] - indx_to_y[task.y]);
+            const cost_t actual = task.actual + cost * dist;
+            pq.push({ next_x, next_y, actual, actual + cost_to_end(next_x, next_y) });
         }
         // down
-        if (cur_y != 0 && !visited[calc_indx(cur_y - 1, cur_x)])
+        if (task.y != 0 && !visited[calc_indx(task.y - 1, task.x)])
         {
-            coord_t next_x = task.x;
-            coord_t next_y = indx_to_y[cur_y - 1];
-            cost_t actual = task.actual + vert_costs[cur_y - 1][cur_x] * abs(next_y - task.y);
-            pq.push({ next_x, next_y, actual, actual + dist_to_end(next_x, next_y) });
+            const size_t next_x = task.x;
+            const size_t next_y = task.y - 1;
+            const cost_t cost = vert_costs[calc_indx(task.y - 1, task.x)];
+            const coord_t dist = abs(indx_to_y[next_y] - indx_to_y[task.y]);
+            const cost_t actual = task.actual + cost * dist;
+            pq.push({ next_x, next_y, actual, actual + cost_to_end(next_x, next_y) });
         }
         // right
-        if (cur_x != grid_x_size - 1 && !visited[calc_indx(cur_y, cur_x + 1)])
+        if (task.x != grid_x_size - 1 && !visited[calc_indx(task.y, task.x + 1)])
         {
-            coord_t next_x = indx_to_x[cur_x + 1];
-            coord_t next_y = task.y;
-            cost_t actual = task.actual + horz_costs[cur_y][cur_x] * abs(next_x - task.x);
-            pq.push({ next_x, next_y, actual, actual + dist_to_end(next_x, next_y) });
+            const size_t next_x = task.x + 1;
+            const size_t next_y = task.y;
+            const cost_t cost = horz_costs[calc_indx(task.y, task.x)];
+            const coord_t dist = abs(indx_to_x[next_x] - indx_to_x[task.x]);
+            const cost_t actual = task.actual + cost * dist;
+            pq.push({ next_x, next_y, actual, actual + cost_to_end(next_x, next_y) });
         }
         // left
-        if (cur_x != 0 && !visited[calc_indx(cur_y, cur_x - 1)])
+        if (task.x != 0 && !visited[calc_indx(task.y, task.x - 1)])
         {
-            coord_t next_x = indx_to_x[cur_x - 1];
-            coord_t next_y = task.y;
-            cost_t actual = task.actual + horz_costs[cur_y][cur_x - 1] * abs(next_x - task.x);
-            pq.push({ next_x, next_y, actual, actual + dist_to_end(next_x, next_y) });
+            const size_t next_x = task.x - 1;
+            const size_t next_y = task.y;
+            const cost_t cost = horz_costs[calc_indx(task.y, task.x - 1)];
+            const coord_t dist = abs(indx_to_x[next_x] - indx_to_x[task.x]);
+            const cost_t actual = task.actual + cost * dist;
+            pq.push({ next_x, next_y, actual, actual + cost_to_end(next_x, next_y) });
         }
     }
 
