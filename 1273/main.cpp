89e3b6a3298ccfdccc4e059d57af65531057d1d9
tallua <talluay@gmail.com>
2020-03-01 22:30:27 +0900

    slow

---
+++ 1273/main.cpp
@@ -0,0 +1,224 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class lazy_segtree
+{
+    struct _internal_data
+    {
+        int lazy;
+        int current;
+    };
+
+    vector<_internal_data> _data;
+    size_t _N;
+    const int _default_val;
+
+public:
+    lazy_segtree(size_t N, int default_val = int())
+        : _N(N), _default_val(default_val)
+    {
+        _data.resize(4 * N, { default_val, default_val });
+    }
+
+    void update_range(size_t begin, size_t end, int delta)
+    {
+        if (begin != end)
+            _update_range(0, 0, _N - 1, begin, end - 1, delta);
+    }
+
+    int query_range(size_t begin, size_t end)
+    {
+        int result = _default_val;
+        _query_range(0, 0, _N - 1, begin, end - 1, result);
+        return result;
+    }
+
+private:
+    void _update_range(size_t x, size_t left, size_t right, size_t front, size_t back, const int& delta)
+    {
+        _update_lazy(x, left, right);
+        if (back < left || right < front)
+            return;
+
+        if (front <= left && right <= back)
+        {
+            _data[x].lazy += delta;
+            return;
+        }
+
+        if (left == right)
+            return;
+
+        size_t l = max(left, front);
+        size_t r = min(right, back);
+        _data[x].current += (delta * (r - l + 1));
+
+        size_t mid = (left + right) / 2;
+        _update_range(2 * x + 1, left, mid, front, back, delta);
+        _update_range(2 * x + 2, mid + 1, right, front, back, delta);
+        _data[x].current = _data[2 * x + 1].current + _data[2 * x + 2].current;
+    }
+
+    void _query_range(size_t x, size_t left, size_t right, size_t front, size_t back, int& result)
+    {
+        if (back < left || right < front)
+            return;
+
+        _update_lazy(x, left, right);
+        if (front <= left && right <= back)
+        {
+            result += _data[x].current;
+            return;
+        }
+
+        if (left == right)
+            return;
+
+        size_t mid = (left + right) / 2;
+        _query_range(2 * x + 1, left, mid, front, back, result);
+        _query_range(2 * x + 2, mid + 1, right, front, back, result);
+    }
+
+    void _update_lazy(size_t x, size_t left, size_t right)
+    {
+        _data[x].current += _data[x].lazy * (right - left + 1);
+
+        if (left != right)
+        {
+            _data[2 * x + 1].lazy += _data[x].lazy;
+            _data[2 * x + 2].lazy += _data[x].lazy;
+        }
+
+        _data[x].lazy = _default_val;
+    }
+};
+
+class segment_tree
+{
+private:
+    vector<int> _data;
+    size_t _N;
+
+public:
+    segment_tree(size_t N, int init_val = int(), int default_val = int())
+        : _N(N)
+    {
+        _data.resize(4 * N, init_val);
+        _init(0, 0, _N - 1);
+    }
+
+    void update(size_t index, int val)
+    {
+        _update(0, 0, _N - 1, index, val);
+    }
+
+    size_t index_query(int val)
+    {
+        return _index_query(0, 0, _N - 1, val);
+    }
+
+private:
+    void _init(size_t x, size_t left, size_t right)
+    {
+        if (left == right)
+            return;
+
+        size_t mid = (left + right) / 2;
+        _init(2 * x + 1, left, mid);
+        _init(2 * x + 2, mid + 1, right);
+        _data[x] = _data[2 * x + 1] + _data[2 * x + 2];
+    }
+
+    void _update(size_t x, size_t left, size_t right, size_t index, int val)
+    {
+        if (index < left || right < index)
+            return;
+
+        if (left == right)
+        {
+            _data[x] = val;
+            return;
+        }
+
+        size_t mid = (left + right) / 2;
+        _update(2 * x + 1, left, mid, index, val);
+        _update(2 * x + 2, mid + 1, right, index, val);
+        _data[x] = _data[2 * x + 1] + _data[2 * x + 2];
+    }
+
+    size_t _index_query(size_t x, size_t left, size_t right, int val)
+    {
+        if (left == right)
+            return left;
+
+        size_t mid = (left + right) / 2;
+        if (_data[2 * x + 1] < val)
+        {
+            return _index_query(2 * x + 2, mid + 1, right, val - _data[2 * x + 1]);
+        }
+        else
+        {
+            return _index_query(2 * x + 1, left, mid, val);
+        }
+    }
+};
+
+int main(int argc, char** argv)
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    lazy_segtree can_info(3000000);
+    segment_tree index_counter(3000000, 1, 0);
+
+    int N;
+    cin >> N;
+
+    vector<size_t> current_heights;
+    current_heights.resize(N, 0);
+
+    for (int n = 0; n < N; ++n)
+    {
+        size_t black;
+        cin >> black;
+        can_info.update_range(current_heights[n], current_heights[n] + black, 1);
+        current_heights[n] += black;
+    }
+
+    for (int n = 0; n < N; ++n)
+    {
+        size_t grey;
+        cin >> grey;
+        can_info.update_range(current_heights[n], current_heights[n] + grey, 2);
+        current_heights[n] += grey;
+    }
+
+    for (int n = 0; n < N; ++n)
+    {
+        size_t white;
+        cin >> white;
+        can_info.update_range(current_heights[n], current_heights[n] + white, 5);
+        current_heights[n] += white;
+    }
+
+    int M;
+    cin >> M;
+
+    while (M--)
+    {
+        int m;
+        cin >> m;
+
+        size_t index = index_counter.index_query(m);
+        index_counter.update(index, 0);
+        int result = can_info.query_range(index, index + 1);
+        //cout << index << " : " << result << endl;
+        cout << result << '\n';
+    }
+
+    return 0;
+}
